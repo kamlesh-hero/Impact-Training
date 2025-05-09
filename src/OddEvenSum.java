ab99d54e5ab263a3f44663e593d54b6ec1f90830
Kamlesh chauhan <kamlesh041512@gmail.com>
2025-05-09 22:41:58 +0530

    Impact training first week

---
+++ src/OddEvenSum.java
@@ -0,0 +1,19 @@
+//import java.util.Scanner;
+//public class OddEvenSum {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number:");
+//        int num = sc.nextInt();
+//        while (num!=0){
+//            int r=num%10;
+//            num=num/10;
+//            if (r%2==0){
+//                System.out.println("Even sum"+sum);
+//            }
+//            else(r%2!=0 && r!=0){
+//                System.out.println("Odd number");
+//            }
+//
+//        }
+//    }
+//}
